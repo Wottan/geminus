60a70466ebbe1fe0fd2a6727719404b309392d67
Ivan Nicolas <ivannsuchan@gmail.com>
2022-11-10 16:26:12 -0300

    First commit

---
+++ src/main/java/com/geminus/GeminusApplication.java
@@ -0,0 +1,13 @@
+package com.geminus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geminus/GeminusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geminus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
